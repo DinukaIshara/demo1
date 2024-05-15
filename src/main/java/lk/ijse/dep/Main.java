e94c980d250757309a1153359d9fba01a4b7256d
DinukaIshara <dinukaphoto692.com>
2024-05-15 14:19:26 +0530

    add my name and two number total

--- src/main/java/lk/ijse/dep/Main.java
+++ src/main/java/lk/ijse/dep/Main.java
@@ -3,5 +3,12 @@ package lk.ijse.dep;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Dinuka Ishara Herath");
+        int num1 = 2;
+        int num2 = 3;
+
+        int tot = num1+num2;
+
+        System.out.println("Total = " + tot);
     }
 }
